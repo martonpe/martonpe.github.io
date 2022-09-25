b60b4a49b0e30490f142d962dfc91324bacf4f27
Peter Marton <martonpe@gmail.com>
2022-09-25 14:46:52 +0200

    Fix boilerplate

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var express = require('express'),
-    path = require('path'),
-    app = express();
+var express = require("express"),
+  path = require("path"),
+  app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set("port", process.env.PORT || 8080);
 
-app.use(express.static('public'));
+app.use(express.static("./"));
 
-app.listen(app.get('port'), function(err) {
+app.listen(app.get("port"), function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('Running on port: ' + app.get('port')); }
+    console.log("Running on port: " + app.get("port"));
+  }
 });
